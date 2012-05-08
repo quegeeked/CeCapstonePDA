c3276ca9a337c1388e0aea07dcbc99e488646a1a
Quamaine Giles <quamaine.giles.08@cnu.edu>
2012-05-08 19:42:04 -0400

    messed with the layout added buttons

--- CeCapstonePDA/src/com/cecapstone/pda/CeCapstonePDAActivity.java
+++ CeCapstonePDA/src/com/cecapstone/pda/CeCapstonePDAActivity.java
@@ -21,77 +21,85 @@ public class CeCapstonePDAActivity extends MapActivity {
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         InitializeMap();
         InitializeMyLocation();
         IntializeLocationManager();
-        Log. i(LogCatTag , "In onCreate" );
-        
-        
-    }
-    
-    public void onStart(){
-        super.onStart();
-    }
-    
-    public void onRestart(){
-        super.onRestart();
-    }
-    
-    public void onResume(){
-        super.onResume();
-    }
-    
-    protected void onPause(){
-        super.onPause();
-    }
-    
-    protected void onStop(){
-        super.onStop();
-    }
-    
-    protected void onDestroy(){
-        super.onDestroy();
-    }
-    
-    private void IntializeLocationManager() {
-        LocationManager mlocManager = 
-                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        LocationListener mlocListener = new MyLocationListener(this, controller);
-        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
-        
-        
+        Log.i(LogCatTag, "In onCreate");
+
     }
 
-    private void InitializeMyLocation(){
+    public void onStart()
+    {
+        super.onStart();
+    }
+
+    public void onRestart()
+    {
+        super.onRestart();
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+    private void IntializeLocationManager()
+    {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener mlocListener = new MyLocationListener(this, controller);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+                mlocListener);
+
+    }
+
+    private void InitializeMyLocation()
+    {
         mypos = new MyLocationOverlay(this, Mapview);
         mypos.enableMyLocation();
         Mapview.getOverlays().add(mypos);
         controller = Mapview.getController();
         mypos.runOnFirstFix(new Runnable() {
-            public void run(){
+            public void run()
+            {
                 controller.setZoom(17);
                 controller.animateTo(mypos.getMyLocation());
             }
         });
-        
+
     }
 
-    private void InitializeMap(){
+    private void InitializeMap()
+    {
         Mapview = (MapView) findViewById(R.id.mapview);
         Mapview.setBuiltInZoomControls(true);
         Mapview.setSatellite(true);
         Mapview.setSaveEnabled(true);
-        
-       
+
     }
 
     @Override
     protected boolean isRouteDisplayed()
     {
-       
+
         return false;
     }
 }
